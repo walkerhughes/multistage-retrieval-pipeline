431d266c5bf3eb7361645bf90eb341150a286f78
Walker Hughes <74113220+walkerhughes@users.noreply.github.com>
2025-12-30 21:02:21 -0700

    initial commit for evals framework

---
+++ evals/schemas/__init__.py
@@ -0,0 +1,15 @@
+"""Evaluation data schemas."""
+
+from evals.schemas.eval_example import (
+    DifficultyLevel,
+    EvalDataset,
+    EvalTask,
+    QuestionType,
+)
+
+__all__ = [
+    "DifficultyLevel",
+    "EvalDataset",
+    "EvalTask",
+    "QuestionType",
+]
